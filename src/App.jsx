9b3c2e7a2ec4f46599bcf4f89ca699c8fd27f822
Ashutosh <ashu57093@gmail.com>
2025-05-24 22:29:18 +0530

    sign/sign up form && Login/header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import './App.css'
+
+import Body from "./components/Body"
 
 function App() {
 
-  return <div className='bg-amber-200'> Helloo</div>
+  return(
+    <Body/>
+  )
 }
 
 export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () =>{
+    return (
+        <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10">
+            <img className="w-44 p-3 " src="/assets/logo.png" alt="logo"/>
+        </div>
+    )
+}
+export default Header;
